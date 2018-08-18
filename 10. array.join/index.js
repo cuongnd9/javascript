2895f16f7587f382e82897f3282e861d1ec967fa
Cuong Duy Nguyen <ndc07.it@gmail.com>
2018-08-18 17:59:46 +0700

    array.join

---
+++ 10. array.join/index.js
@@ -0,0 +1,6 @@
+const numbers=[1,2,3,5,9,8,2,3,12];
+// let strNumbers=numbers.join();
+// let strNumbers=numbers.join('');
+// let strNumbers=numbers.join(', ');
+let strNumbers=numbers.join('$');
+strNumbers;
